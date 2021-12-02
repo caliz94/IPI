69b66bfb3014cb3824dfa803873b28aed80aeb13
testblues001 <testblues001@gmail.com>
2021-12-01 23:48:58 -0600

    Se procedio a corregir script de BD y consolidar Capa Datos Clientes

--- IPI_CasoPract_Fase2_V2/IPI_CasoPract_Fase2_V2/CD_Datos/CD_Clientes.cs
+++ IPI_CasoPract_Fase2_V2/IPI_CasoPract_Fase2_V2/CD_Datos/CD_Clientes.cs
@@ -12,22 +12,94 @@ namespace CD_Datos
     {
         private CD_Conexion Conexion = new CD_Conexion();
 
-       
+
         SqlCommand cmd = new SqlCommand();
 
-        public void nuevo_cliente(string NombreCliente,string Saldo,string LimiteCredito,string Descuento,int Activo )
+
+        //Metodo Nuevo Cliente
+        public void nuevo_cliente(string NombreCliente, string Saldo, string LimiteCredito, string Descuento, int Activo)
         {
             cmd.Connection = Conexion.abrircadena();
-            cmd.CommandText = "SP_NuevoCliente";
+            cmd.CommandText = "sp_nuevoCliente";
             cmd.CommandType = CommandType.StoredProcedure;
             cmd.Parameters.AddWithValue("@NombreCliente", NombreCliente);
             cmd.Parameters.AddWithValue("@Saldo", Saldo);
             cmd.Parameters.AddWithValue("@LimiteCredito", LimiteCredito);
             cmd.Parameters.AddWithValue("@Descuento", Descuento);
-            cmd.Parameters.AddWithValue("@Activo", Activo);
             cmd.ExecuteNonQuery();
             cmd.Parameters.Clear();
-            Conexion.cerrarcadena();            
+            Conexion.cerrarcadena();
+        }
+
+
+        //Editar Cliente
+        public void act_cliente(int IdCliente, string NombreCliente, string Saldo, string @Limite, string Descuento, int activo)
+        {
+            cmd.Connection = Conexion.abrircadena();
+            cmd.CommandText = "sp_ActualizarCliente";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@IdCliente", IdCliente);
+            cmd.Parameters.AddWithValue("@NombreCliente", NombreCliente);
+            cmd.Parameters.AddWithValue("@Saldo", Saldo);
+            cmd.Parameters.AddWithValue("@@Limite", @Limite);
+            cmd.Parameters.AddWithValue("@Descuento", Descuento);
+            cmd.Parameters.AddWithValue("@activo", activo);
+            cmd.ExecuteNonQuery();
+            cmd.Parameters.Clear();
+            Conexion.cerrarcadena();
+        }
+
+
+        //Eliminar Cliente
+        public void del_client(int @IdCliente)
+        {
+            cmd.Connection = Conexion.abrircadena();
+            cmd.CommandText = "sp_eliminarCliente";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@IdCliente", IdCliente);
+            cmd.ExecuteNonQuery();
+            cmd.Parameters.Clear();
+            Conexion.cerrarcadena();
+        }
+
+
+        //Mostrar Todos los Clientes 
+        SqlDataReader leerdata;
+        DataTable tabla = new DataTable();
+        public DataTable MostrarClientes()
+        {
+            cmd.Connection = Conexion.abrircadena();
+            cmd.CommandText = "sp_mostrarClientes";
+            cmd.CommandType = CommandType.StoredProcedure;
+            leerdata = cmd.ExecuteReader();
+            tabla.Load(leerdata);
+            Conexion.cerrarcadena();
+            return tabla;
+        }
+
+
+        //Mostrar Clientes Inactivos
+        public DataTable ClientesInactivos()
+        {
+            cmd.Connection = Conexion.abrircadena();
+            cmd.CommandText = "sp_mostrarClientesInactivos";
+            cmd.CommandType = CommandType.StoredProcedure;
+            leerdata = cmd.ExecuteReader();
+            tabla.Load(leerdata);
+            Conexion.cerrarcadena();
+            return tabla;
+        }
+        //mostrar clientes activos     
+
+        public DataTable ClientesActivos()
+        {
+            cmd.Connection = Conexion.abrircadena();
+            cmd.CommandText = "sp_mostrarClientesActivos";
+            cmd.CommandType = CommandType.StoredProcedure;
+            leerdata = cmd.ExecuteReader();
+            tabla.Load(leerdata);
+            Conexion.cerrarcadena();
+            return tabla;
         }
     }
 }
--- TABLAS Y PROC.bak.sql
+++ TABLAS Y PROC.bak.sql
@@ -1,88 +1,85 @@
---USE master
---GO
+USE master
+GO
 
---IF DB_ID('IPICASOPRACTICO') IS NOT NULL
---BEGIN
---	DROP DATABASE IPICASOPRACTICO
---END
+IF DB_ID('IPICASOPRACTICO') IS NOT NULL
+BEGIN
+	DROP DATABASE IPICASOPRACTICO
+END
 
---CREATE DATABASE IPICASOPRACTICO
---GO
+CREATE DATABASE IPICASOPRACTICO
+GO
 
---USE IPICASOPRACTICO
---GO
+USE IPICASOPRACTICO
+GO
 
---CREATE TABLE Cliente
---(
---	IdCliente INT PRIMARY KEY,
---	NombreCliente VARCHAR(100) NOT NULL,
---	Saldo MONEY NOT NULL,
---	LimiteCredito MONEY CHECK(LimiteCredito <= 3000000) NOT NULL,
---	Descuento MONEY NOT NULL,
---	Activo BIT not null
---)
---GO
+CREATE TABLE Cliente
+(
+	IdCliente INT PRIMARY KEY,
+	NombreCliente VARCHAR(100) NOT NULL,
+	Saldo MONEY NOT NULL,
+	LimiteCredito MONEY CHECK(LimiteCredito <= 3000000) NOT NULL,
+	Descuento MONEY NOT NULL,
+	Activo BIT not null
+)
+GO
 
---CREATE TABLE Direcciones
---(
---	IdDireccion INT PRIMARY KEY NOT NULL,
---	IdCliente INT FOREIGN KEY REFERENCES dbo.Cliente(IdCliente) NOT NULL,
---	Calle VARCHAR(25) NOT NULL,
---	Barrio VARCHAR(80) NOT NULL,
---	Distrito VARCHAR(250) NOT NULL,
---	Activo BIT NOT NULL
---)
---GO
+CREATE TABLE Direcciones
+(
+	IdDireccion INT PRIMARY KEY NOT NULL,
+	IdCliente INT FOREIGN KEY REFERENCES dbo.Cliente(IdCliente) NOT NULL,
+	Calle VARCHAR(25) NOT NULL,
+	Barrio VARCHAR(80) NOT NULL,
+	Distrito VARCHAR(250) NOT NULL,
+	Activo BIT NOT NULL
+)
+GO
 
 
---CREATE TABLE Fabrica
---(
---  IdFabrica INT PRIMARY KEY,
---  NombreFabrica VARCHAR(50) NOT NULL,
---  --CodArticulo INT UNIQUE NOT NULL,
---  --Descripcion_Articulo VARCHAR(250) NOT NULL,
---  --Existencias INT NOT NULL,
---  Telefono VARCHAR(13) NOT NULL,
---  Activo BIT NOT NULL
---)
---GO
+CREATE TABLE Fabrica
+(
+  IdFabrica INT PRIMARY KEY,
+  NombreFabrica VARCHAR(50) NOT NULL,
+  Telefono VARCHAR(13) NOT NULL,
+  Activo BIT NOT NULL
+)
+GO
 
---CREATE TABLE Articulo
---(
---	IdArticulo INT PRIMARY KEY,
---	--CodArticulo INT UNIQUE FOREIGN KEY REFERENCES Fabrica(CodArticulo) NOT NULL,
---	Descripción_Articulo VARCHAR(250) NOT NULL,
---	Existencias INT NOT NULL,
---	PrecioUnitario MONEY NOT NULL,
---	IdFabrica INT FOREIGN KEY REFERENCES dbo.Fabrica(IdFabrica) NOT NULL,
---	ArticulosProvistos INT NOT NULL,
---	NoFabricasAlternativas INT NOT NULL
---)
---GO
+CREATE TABLE Articulo
+(
+	IdArticulo INT PRIMARY KEY,
+	Descripción_Articulo VARCHAR(250) NOT NULL,
+	Existencias INT NOT NULL,
+	PrecioUnitario MONEY NOT NULL,
+	IdFabrica INT FOREIGN KEY REFERENCES dbo.Fabrica(IdFabrica) NOT NULL,
+	ArticulosProvistos INT NOT NULL,
+	NoFabricasAlternativas INT NOT NULL
+)
+GO
 
---CREATE TABLE Pedido
---(
---	IdPedido INT PRIMARY KEY,
---    IdCliente INT FOREIGN KEY REFERENCES dbo.Cliente(IdCliente) NOT NULL,
---	IdDireccion INT FOREIGN KEY REFERENCES dbo.Direcciones(IdDireccion) NOT NULL,
---	FechaPedido TIMESTAMP NOT NULL
---)
---GO
+CREATE TABLE Pedido
+(
+	IdPedido INT PRIMARY KEY,
+    IdCliente INT FOREIGN KEY REFERENCES dbo.Cliente(IdCliente) NOT NULL,
+	IdDireccion INT FOREIGN KEY REFERENCES dbo.Direcciones(IdDireccion) NOT NULL,
+	FechaPedido DATETIME NOT NULL,
+	Activo BIT NOT NULL
+)
+GO
 
---CREATE TABLE Detalle_Pedido
---(
---	IdArticulo INT FOREIGN KEY REFERENCES dbo.Articulo(IdArticulo),
---    IdPedido INT FOREIGN KEY REFERENCES dbo.Pedido(IdPedido),
---	Cantidad INT NOT NULL,
---	Fabrica INT FOREIGN KEY REFERENCES dbo.Fabrica(IdFabrica) NOT NULL
---	PRIMARY KEY(IdArticulo,IdPedido)
---)
---GO
+CREATE TABLE Detalle_Pedido
+(
+	IdArticulo INT FOREIGN KEY REFERENCES dbo.Articulo(IdArticulo),
+    IdPedido INT FOREIGN KEY REFERENCES dbo.Pedido(IdPedido),
+	Cantidad INT NOT NULL,
+	Fabrica INT FOREIGN KEY REFERENCES dbo.Fabrica(IdFabrica) NOT NULL
+	PRIMARY KEY(IdArticulo,IdPedido)
+)
+GO
 
 
 --PROCEDIMIENTOS ALMACENADOS
 --***************************************** FABRICAS**********************************************************************
-ALTER PROCEDURE sp_mostrarFabricas
+CREATE PROCEDURE sp_mostrarFabricas
 AS
 BEGIN
     SELECT * FROM dbo.Fabrica 
@@ -105,7 +102,7 @@ GO
 
 CREATE PROCEDURE sp_eliminarFabrica
 (
-@IdFabrica INT
+	@IdFabrica INT
 )
 AS
 BEGIN
@@ -115,12 +112,12 @@ BEGIN
 END
 GO
 
-CREATE PROCEDURE sp_actualizarCliente
+CREATE PROCEDURE sp_actualizarFabrica
 (
-@IdFabrica INT,
-@NombreFabrica VARCHAR(50),
-@Telefono VARCHAR(13),
-@Activo BIT
+	@IdFabrica INT,
+	@NombreFabrica VARCHAR(50),
+	@Telefono VARCHAR(13),
+	@Activo BIT
 )
 AS
 BEGIN
@@ -130,7 +127,7 @@ BEGIN
 END
 GO
 
-ALTER PROCEDURE sp_nuevaFabrica
+CREATE PROCEDURE sp_nuevaFabrica
 (
 	@NombreFabrica VARCHAR(50),
 	@Telefono VARCHAR(13)
@@ -150,7 +147,8 @@ BEGIN
 	)
 END
 
-EXEC sp_nuevaFabrica 'CURACAO', '22685303'
+GO
+--EXEC sp_nuevaFabrica 'CURACAO', '22685303'
 
 
 --*******************************************CLIENTES**********************************************************************
@@ -175,7 +173,7 @@ BEGIN
 END
 GO
 
-CREATE PROCEDURE sp_EliminarCliente
+CREATE PROCEDURE sp_eliminarCliente
 (
 @IdCliente INT
 )
@@ -205,7 +203,7 @@ END
 GO
 
 
-CREATE PROCEDURE SP_NuevoCliente
+CREATE PROCEDURE sp_nuevoCliente
 (
 	@NombreCliente VARCHAR(100),
 	@Saldo MONEY ,
@@ -215,7 +213,7 @@ CREATE PROCEDURE SP_NuevoCliente
 AS
 BEGIN
 	DECLARE @IdCliente INT
-	SET @IdCliente = (select max(isnull(IdCliente,0)+1) from Cliente)
+	SET @IdCliente = (select ISNULL(MAX(IdCliente),0)+1 from Cliente)
 
 	INSERT INTO dbo.Cliente
 	VALUES
@@ -228,38 +226,41 @@ BEGIN
 		1  -- Activo - bit
 	)
 END
+GO
 
-
-EXEC dbo.SP_NuevoCliente @NombreCliente = 'Andres Josias Castro Gutierrez',    -- varchar(25)
-                        @Saldo = '10000',   -- varchar(80)
-                        @LimiteCredito = '30000',  -- varchar(250)
-						@Descuento = '300'  -- varchar(250)
-						GO
+--EXEC dbo.SP_NuevoCliente @NombreCliente = 'Andres Josias Castro Gutierrez',    -- varchar(25)
+--                        @Saldo = '10000',   -- varchar(80)
+--                        @LimiteCredito = '30000',  -- varchar(250)
+--						@Descuento = '300'  -- varchar(250)
+--						GO
 
 --***************************************DIRECCIONES*****************************************
-ALTER PROCEDURE sp_direcciones
-@idcliente INT,
-@calle VARCHAR(25),
-@barrio VARCHAR(80),
-@distrito VARCHAR(250)
+CREATE PROCEDURE sp_direcciones
+(
+	@idcliente INT,
+	@calle VARCHAR(25),
+	@barrio VARCHAR(80),
+	@distrito VARCHAR(250)
+)
 AS
 BEGIN
 	DECLARE @iddireccion INT
 
-	SET @iddireccion = (select isnull(max(IdDireccion),1) from Direcciones)
+	SET @iddireccion = (select isnull(max(IdDireccion),0)+1 from Direcciones)
 
 	INSERT INTO dbo.Direcciones (IdDireccion,IdCliente, Calle, Barrio, Distrito, Activo)
 	VALUES (@iddireccion, @idcliente, @calle, @barrio, @distrito, 1)
 END
+GO
 
 
-select * from Cliente
-SELECT * FROM Direcciones
-SELECT * FROM Fabrica
+--select * from Cliente
+--SELECT * FROM Direcciones
+--SELECT * FROM Fabrica
 
-EXEC dbo.sp_direcciones @idcliente = 1, -- int
-                        @calle = 'Vida Nueva',    -- varchar(25)
-                        @barrio = 'La cruz',   -- varchar(80)
-                        @distrito = 'II'  -- varchar(250)
+--EXEC dbo.sp_direcciones @idcliente = 1, -- int
+--                        @calle = 'Vida Nueva',    -- varchar(25)
+--                        @barrio = 'La cruz',   -- varchar(80)
+--                        @distrito = 'II'  -- varchar(250)
 						
-						GO
+--						GO
