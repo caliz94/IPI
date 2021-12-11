0a71f779e645bba5011e35f8a709faf0ead9657c
DANNY <dannycaliz94@gmail.com>
2021-12-11 07:40:52 -0600

    gter

--- IPI_CasoPract_Fase2_V2/IPI_CasoPract_Fase2_V2/CP_Presentacion/Reporte/CP_Factura.Designer.cs
+++ IPI_CasoPract_Fase2_V2/IPI_CasoPract_Fase2_V2/CP_Presentacion/Reporte/CP_Factura.Designer.cs
@@ -29,15 +29,18 @@
         private void InitializeComponent()
         {
             this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.rp_Factura1 = new CP_Presentacion.rp_Factura();
             this.SuspendLayout();
             // 
             // crystalReportViewer1
             // 
             this.crystalReportViewer1.ActiveViewIndex = -1;
             this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
             this.crystalReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.crystalReportViewer1.Location = new System.Drawing.Point(0, 0);
             this.crystalReportViewer1.Name = "crystalReportViewer1";
+            this.crystalReportViewer1.ReportSource = this.rp_Factura1;
             this.crystalReportViewer1.Size = new System.Drawing.Size(385, 261);
             this.crystalReportViewer1.TabIndex = 0;
             // 
@@ -56,5 +59,6 @@
         #endregion
 
         private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
+        private rp_Factura rp_Factura1;
     }
 }
